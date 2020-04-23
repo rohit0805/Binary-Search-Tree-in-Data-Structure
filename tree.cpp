d246e6b583fb5e4bc41b60470c59f0520a4c4009
Rohit <rohit5822540@gmail.com>
2020-04-23 21:09:54 +0530

    Least Common Ancestor of BST

--- tree.cpp
+++ tree.cpp
@@ -17,75 +17,40 @@ void inorder(struct node *root){
 	cout<<root->data<<" ";
 	inorder(root->right);
 }
-struct node* insert(struct node *root,int data){
-	if(root==NULL)
-		return create(data);
-	else if(root->data>=data){
-		root->left=insert(root->left,data);
+int lca(struct node *root,int d1,int d2){
+	if(root->data>=d1 && root->data<=d2)
+		return root->data;
+	else if(root->data>d1 &&root->data>d2){
+		return lca(root->left,d1,d2);
 	}
-	else{
-		root->right=insert(root->right,data);
-	}
-	return root;
+	else
+		return lca(root->right,d1,d2);
+	return root->data;
 }
-struct node* findmin(struct node *root){
-	struct node *current=root;
-	while(current->left!=NULL){
-		current=current->left;
-	}
-	return current;
+struct node* insert(struct node* node, int key)
+{
+    if (node == NULL) 
+        return create(key);
+    if (key < node->data)
+        node->left = insert(node->left, key);
+    else
+        node->right = insert(node->right, key);
+    return node;
 }
-struct node* deletenode(struct node *root,int data){
-	if(root==NULL)
-		return NULL;
-	else if(root->data>data)
-		root->left=deletenode(root->left,data);
-	else if(root->data<data)
-		root->right=deletenode(root->right,data);
-	else{
-		//if the deletednode is leaf or have only one child
-		if(root->left==NULL){
-			struct node *temp=root->right;
-			free(root);
-			return temp;
-		}
-		else if(root->right==NULL){
-			struct node *temp=root->left;
-			free(root);
-			return temp;
-		}
-		struct node *temp=findmin(root->right);
-		root->data=temp->data;
-		root->right=deletenode(root->right,temp->data);
-	}
-	return root;
-}
+
 int main(){
 	struct node *root=NULL;
-	root=insert(root,50);
-	insert(root,30);
-	insert(root,20);
-	insert(root,40);	
-	insert(root,70);	
-	insert(root,60);	
-	insert(root,80);
+	root=insert(root,20);
+	insert(root,8);
+	insert(root,22);
+	insert(root,4);	
+	insert(root,12);	
+	insert(root,10);	
+	insert(root,14);
 	cout<<"Inorder traversal of tree\n";	
 	inorder(root);
-	
-    cout<<"\nDelete 20\n";
-    root = deletenode(root, 20);
-    cout<<"Inorder traversal of the modified tree \n";
-    inorder(root);
-
-    cout<<"\nDelete 30\n";
-    root = deletenode(root, 30);
-    cout<<"Inorder traversal of the modified tree \n";
-    inorder(root);
-
-    cout<<"\nDelete 50\n";
-    root = deletenode(root, 50);
-    cout<<"Inorder traversal of the modified tree \n";
-    inorder(root);
-
+	cout<<"LCA of 10 and 14: "<<lca(root,10,14)<<endl;
+	cout<<"LCA of 8 and 14: "<<lca(root,8,14)<<endl;
+	cout<<"LCA of 10 and 22: "<<lca(root,10,22)<<endl;
 	return 0;
 }
