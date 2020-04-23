a368f934fdcfb4c60fe742fd1b2d586d7f878fa5
Rohit <rohit5822540@gmail.com>
2020-04-23 20:57:38 +0530

    Deletion in a Binary Search Tree

--- tree.cpp
+++ tree.cpp
@@ -5,24 +5,22 @@ struct node{
 	struct node *left,*right;
 };
 struct node *create(int data){
-	struct node *newnode=new node;
-	newnode->left=newnode->right=NULL;
+	struct node *newnode=(struct node*)malloc(sizeof(struct node));
 	newnode->data=data;
+	newnode->left=newnode->right=NULL;
 	return newnode;
 }
-
 void inorder(struct node *root){
 	if(root==NULL)
-		return;
+		return ;
 	inorder(root->left);
 	cout<<root->data<<" ";
 	inorder(root->right);
 }
 struct node* insert(struct node *root,int data){
-	if(root==NULL){
+	if(root==NULL)
 		return create(data);
-	}
-	if(root->data>=data){
+	else if(root->data>=data){
 		root->left=insert(root->left,data);
 	}
 	else{
@@ -30,30 +28,64 @@ struct node* insert(struct node *root,int data){
 	}
 	return root;
 }
-bool search(struct node *root,int data){
-	if(root==NULL)
-		return false;
-	if(root->data==data){
-		return true;
+struct node* findmin(struct node *root){
+	struct node *current=root;
+	while(current->left!=NULL){
+		current=current->left;
 	}
-	if(root->data>=data){
-		return search(root->left,data);
-	}
-	else{
-		return search(root->right,data);
-	}
+	return current;
 }
-
+struct node* deletenode(struct node *root,int data){
+	if(root==NULL)
+		return NULL;
+	else if(root->data>data)
+		root->left=deletenode(root->left,data);
+	else if(root->data<data)
+		root->right=deletenode(root->right,data);
+	else{
+		//if the deletednode is leaf or have only one child
+		if(root->left==NULL){
+			struct node *temp=root->right;
+			free(root);
+			return temp;
+		}
+		else if(root->right==NULL){
+			struct node *temp=root->left;
+			free(root);
+			return temp;
+		}
+		struct node *temp=findmin(root->right);
+		root->data=temp->data;
+		root->right=deletenode(root->right,temp->data);
+	}
+	return root;
+}
 int main(){
 	struct node *root=NULL;
 	root=insert(root,50);
 	insert(root,30);
 	insert(root,20);
-	insert(root, 40);
-    insert(root, 70);
-    insert(root, 60);
-    insert(root, 80);
-    cout<<search(root,700)<<endl;
+	insert(root,40);	
+	insert(root,70);	
+	insert(root,60);	
+	insert(root,80);
+	cout<<"Inorder traversal of tree\n";	
+	inorder(root);
+	
+    cout<<"\nDelete 20\n";
+    root = deletenode(root, 20);
+    cout<<"Inorder traversal of the modified tree \n";
     inorder(root);
+
+    cout<<"\nDelete 30\n";
+    root = deletenode(root, 30);
+    cout<<"Inorder traversal of the modified tree \n";
+    inorder(root);
+
+    cout<<"\nDelete 50\n";
+    root = deletenode(root, 50);
+    cout<<"Inorder traversal of the modified tree \n";
+    inorder(root);
+
 	return 0;
 }
